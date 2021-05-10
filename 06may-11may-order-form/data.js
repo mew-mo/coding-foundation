d01353ce05f43fcb4683786a5ebd40d5d78ee5d0
Monique Tickle <270032361@bun2-206-24.acg.ac.nz>
2021-05-11 11:59:30 +1200

    each loop

---
+++ 06may-11may-order-form/data.js
@@ -0,0 +1,11 @@
+
+  var promotions = {
+
+  		lessThan15: ['1 fries', '1 hash brown coupon', '1 free sauce'],
+
+  		moreThan15: ['1 burger', '1 drink', '1 ice-cream'],
+  		// more than 15 and less than 20
+
+  		moreThan20: ['1 burger', '2 drinks', '2 ice-creams', '1 car wash ticket']
+
+  };
