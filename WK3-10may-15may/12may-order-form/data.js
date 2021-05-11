3f25e5f7d21f9b6e9681312dd93121a0f5c6b789
Monique Tickle <270032361@bun2-206-24.acg.ac.nz>
2021-05-12 11:51:23 +1200

    order form wooh

---
+++ WK3-10may-15may/12may-order-form/data.js
@@ -0,0 +1,11 @@
+
+  var promotions = {
+
+  		lessThan15: ['1x Fries', '1x Hash Brown Coupon', '1x Free Sauce'],
+
+  		moreThan15: ['1x Burger', '1x Drink', '1x Ice-Cream'],
+  		// more than 15 and less than 20
+
+  		moreThan20: ['1x Burger', '2x Drinks', '2x Ice-Creams', '1x Car Wash Ticket']
+
+  };
